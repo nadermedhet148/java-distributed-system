191a10c1aa838efc0e85e4ba2c1ad1a56a130429
nader <nader@192.168.1.12>
2021-01-19 19:15:45 +0200

    [feat] add user service

--- run.bash
+++ run.bash
@@ -1,5 +1,16 @@
 cd naming-service
+./mvnw package
 docker build . -t naming-service
+cd ..
 cd config-service
+./mvnw package
 docker build . -t config-service
+cd ..
+cd user-service
+./mvnw package
+docker build . -t user-service
+cd ..
+
+
+
 
